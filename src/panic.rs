abce73f59b58e71034d810fcf38d88a633b69c27
Emmett Rayes <emmett.rayes@icloud.com>
2023-12-31 20:01:52 +0100

    Disable panic handler for boot_uefi

--- src/panic.rs
+++ src/panic.rs
@@ -1,5 +1,10 @@
+//! Until the bootloader is compiled separately from the kernel,
+//! a panic handler is needed only for legacy boot as the uefi crate comes with its own panic handler.
+
+#[cfg(not(feature = "boot_uefi"))]
 use core::panic::PanicInfo;
 
+#[cfg(not(feature = "boot_uefi"))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
