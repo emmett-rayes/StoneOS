21f7c5eba9e956d2d4d5196727429b390a9e297e
Emmett Rayes <emmett.rayes@icloud.com>
2024-01-03 03:58:40 +0100

    Implement print and fmt macros

---
+++ src/print.rs
@@ -0,0 +1,24 @@
+use core::fmt;
+
+use crate::console::ConsoleWrapper;
+use crate::console::Write;
+
+impl fmt::Write for ConsoleWrapper {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for char in s.chars() {
+            self.0.write(char);
+        }
+        Ok(())
+    }
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => (core::fmt::Write::write_fmt(&mut *$crate::console::CONSOLE.lock(), format_args!($($arg)*)).unwrap());
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
