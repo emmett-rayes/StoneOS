a0bd99d3f696c7521105732b21370af37214c79a
Emmett Rayes <emmett.rayes@icloud.com>
2024-01-13 17:31:15 +0100

    Disable UEFI logger and panic handler

--- src/panic.rs
+++ src/panic.rs
@@ -1,15 +1,9 @@
-/// Until the bootloader is compiled separately from the kernel,
-/// a panic handler is needed only for non-uefi boot as the uefi crate comes with its own panic handler.
-#[cfg(not(feature = "boot_uefi"))]
-mod not_boot_uefi_panic {
-    use core::panic::PanicInfo;
+use crate::cpu::park_core;
+use crate::println;
+use core::panic::PanicInfo;
 
-    use crate::cpu::park_core;
-    use crate::println;
-
-    #[panic_handler]
-    fn panic(info: &PanicInfo) -> ! {
-        println!("{}", info);
-        park_core();
-    }
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    park_core();
 }
