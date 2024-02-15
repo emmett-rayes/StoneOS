bca364d8e52356ef40646a9521668b0ef3a1377a
Emmett Rayes <emmett.rayes@icloud.com>
2024-02-15 17:14:02 +0100

    Enforce traits on Bsp implementation

--- src/bsp.rs
+++ src/bsp.rs
@@ -1,5 +1,14 @@
+use crate::console::Console;
+use crate::cpu::Cpu;
+
 #[cfg(feature = "bsp_rpi4")]
-pub use crate::bsp::rpi4::RPi4 as Bsp;
+use crate::bsp::rpi4::RPi4 as BspImpl;
 
 #[cfg(feature = "bsp_rpi4")]
 pub mod rpi4;
+
+trait IBsp: Console + Cpu {}
+
+impl IBsp for BspImpl {}
+
+pub type Bsp = BspImpl;
